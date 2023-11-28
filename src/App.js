e127bf6dcadcb9a1a5c3937b9fba3834cd4bd47e
ilsy23 <ilsysli@naver.com>
2023-11-28 14:29:41 +0900

    #9. 로그인 기능 구현

--- src/App.js
+++ src/App.js
@@ -6,27 +6,33 @@ import Footer from './components/layout/Footer';
 import { Route, Routes } from 'react-router-dom';
 import Login from './components/user/Login';
 import Join from './components/user/Join';
+import { AuthContextProvider } from './utils/AuthContext';
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <Routes>
-        <Route
-          path='/'
-          element={<TodoTemplate />}
-        />
-        <Route
-          path='/login'
-          element={<Login />}
-        />
-        <Route
-          path='join'
-          element={<Join />}
-        />
-      </Routes>
-      <Footer />
-    </>
+    // 데이터를 전달하고자 하는 자식 컴포넌트를 Provider로 감싼다.
+    <AuthContextProvider>
+      <div className='wrapper'>
+        <Header />
+        <div className='content-wrapper'>
+          <Routes>
+            <Route
+              path='/'
+              element={<TodoTemplate />}
+            />
+            <Route
+              path='/login'
+              element={<Login />}
+            />
+            <Route
+              path='join'
+              element={<Join />}
+            />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </AuthContextProvider>
   );
 };
 
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,22 @@
 import { AppBar, Grid, Toolbar, Typography } from '@mui/material';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Header.scss';
+import { AuthContext } from '../../utils/AuthContext';
 
 const Header = () => {
+  const redirection = useNavigate();
+
+  // AuthContext에서 로그인 상태 가져오기
+  const { isLoggedIn, userName, onLogout } = useContext(AuthContext);
+
+  // 로그아웃 핸들러
+  const logoutHandler = () => {
+    // AuthContext의 onLogout 함수를 호출하여 로그인 상태를 업데이트 합니다.
+    onLogout();
+    redirection('/login');
+  };
+
   return (
     <AppBar
       position='fixed'
@@ -28,15 +41,28 @@ const Header = () => {
               }}
             >
               <Typography variant='h4'>
-                <Link to='/'>오늘의 할일</Link>
+                <Link to='/'>
+                  {isLoggedIn ? userName + '님' : '오늘'}의 할 일
+                </Link>
               </Typography>
             </div>
           </Grid>
 
           <Grid item>
             <div className='btn-group'>
-              <Link to='/login'>로그인</Link>
-              <Link to='/join'>회원가입</Link>
+              {isLoggedIn ? (
+                <button
+                  className='logout-btn'
+                  onClick={logoutHandler}
+                >
+                  로그아웃
+                </button>
+              ) : (
+                <>
+                  <Link to='/login'>로그인</Link>
+                  <Link to='/join'>회원가입</Link>
+                </>
+              )}
             </div>
           </Grid>
         </Grid>
